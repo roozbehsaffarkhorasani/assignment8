e566374f6197b554508fc1f2fcb614878caabdd2
roozbeh safar <88191745+roozbehsaffarkhorasani@users.noreply.github.com>
2021-12-12 15:48:57 +0330

    Add files via upload

---
+++ Fac.py
@@ -0,0 +1,15 @@
+a=input("wut is ur favorite film")
+b=input("wut is ur favorite anime")
+c=input("wut is favorite anime charecter")
+d=input(",wut is ur name")
+e=input("how old are u")
+class Person:
+    def__init__(self , ff , fa , fac,   name , age):
+        self.ff=ff
+        self.fa=fa
+        self.fac=fac
+        self.name=name
+        self.age=age
+    I=Person(a , b , c , d , e)
+       print(I.ff I.fa I.fac I.name I.age)
+    
